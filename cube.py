de0d1cdfed39c225314c4721be19d8eb1cc329a8
ConnorParker <connor.parker.737@gmail.com>
2021-03-19 14:16:53 -0400

    Update cube.py

---
+++ cube.py
@@ -0,0 +1,25 @@
+class Cube:
+
+    def move(self, face, direction):
+        pass
+
+    def solved():
+        pass
+
+    def shuffle():
+        pass
+
+    def update_angle(delta_angle, axis):
+        pass
+
+    def notify():
+        pass
+
+    def get_state()
+        pass
+
+    def undo():
+        pass
+
+    def set_cube(n):
+        pass
