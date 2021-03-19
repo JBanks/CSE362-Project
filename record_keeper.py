354edadfd9ca9839e848ac0537bbecd68f2c4a76
ConnorParker <connor.parker.737@gmail.com>
2021-03-19 14:52:58 -0400

    Update record_keeper.py

--- record_keeper.py
+++ record_keeper.py
@@ -1,16 +1,43 @@
 class RecordKeeper:
 
+    def __init__(self):
+        self.is_high_score = False
+        self.best_times = []
+        self.best_moves = []
+        self.high_scores_list = []
+
     def get_best_times(self):
+        """
+        This will return a list of all the times stored in the best_times list
+        :return: List<float> best_times
+        """
         pass
 
-    def get_best_scores(self):
+    def get_best_moves(self):
+        """
+        This will return a list of all the moves stored in the best_moves list
+        :return: List<int> best_moves
+        """
         pass
 
-    def check_high_score(self):
+    def check_high_score(self, moves, time):
+        """
+        This will return true if the current score is a greater than all others
+        in high_scores_list, or false otherwise
+        :return: bool is_high_score
+        """
         pass
 
-    def post_high_score(self):
+    def post_high_score(self, moves, time):
+        """
+        This will enter the current score into high_scores_list
+        :return: void
+        """
         pass
 
     def clear(self):
+        """
+        This will delete all entities in high_scores_list
+        :return: void
+        """
         pass
