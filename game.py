14a232ac7e6b7c807e2be9a2d87921b152c3ed05
Jeremy Banks <JBanks@users.noreply.github.com>
2021-03-19 15:07:58 -0300

    Initial Upload
    Create files and provide some definitions

---
+++ game.py
@@ -0,0 +1,50 @@
+
+
+class Game:
+    def keypress(self, key):
+        pass
+
+    def click(self, x, y):
+        pass
+
+    def drag(self, start_x, start_y, x, y):
+        pass
+
+    def update(self, move):
+        pass
+
+    def get_movement(self, key):
+        pass
+
+    def get_rotation(self, key):
+        pass
+
+    def state_update(self, key):
+        pass
+
+    def reset(self):
+        pass
+
+    def quit(self):
+        pass
+
+    def check_movement(self, key):
+        pass
+
+    def check_rotation(self, key):
+        pass
+
+    def save(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def ask_location(self):
+        pass
+
+    def insert_location(self, location):
+        pass
+
+    def set_cube(self, n):
+        pass
