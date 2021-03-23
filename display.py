ac4a1f5d159b918e08044a2ef1ce7e860d8dffd7
LoulouFer <78125659+LoulouFer@users.noreply.github.com>
2021-03-23 10:50:49 -0400

    added a method to display

--- display.py
+++ display.py
@@ -1,4 +1,17 @@
+import pygame
+from pygame.locals import *
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
 class Display:
+    def __init__(self):
+        xrot = yrot = 0
+
+
+    def display(self):
+        pass
 
     def update_move(self, move):
         pass
