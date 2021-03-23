6af51aa28d9884ecab65c6ce49ac438967cb682c
LoulouFer <78125659+LoulouFer@users.noreply.github.com>
2021-03-23 11:36:31 -0400

    not done yet : set_cube method

--- cube.py
+++ cube.py
@@ -1,5 +1,7 @@
-import math
-
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 import math
 
 class Cube:
@@ -10,6 +12,16 @@ class Cube:
         self.phi = math.pi/8  # start at pi/8
         self.theta = math.pi/8
         self.active_face = 0
+        self.cube_size = 2
+
+        pygame.init()
+        display = (800, 600)
+        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+        gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+
+        glTranslatef(-1.5, -2.0, -10)
+        glEnable(GL_DEPTH_TEST)
 
     def move(self, face, direction):
         """
@@ -75,9 +87,76 @@ class Cube:
 
     def set_cube(self, n):
         """
-
-        :param
-        :param
-        :return:
+        this method is going to set the cube to be displayed
+        :param n : the dimension of the cube.
+        :return: void
         """
-        pass
+        glPushMatrix()
+
+        # front face : red
+        glColor(1,0,0)
+        glBegin(GL_QUADS)
+        glVertex3f(self.cube_size/2,self.cube_size/2,self.cube_size/2)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glEnd()
+        # back face : orange
+        glColor3f(1, 176.0 / 255.0, 5.0 / 255.0)
+        glBegin(GL_QUADS)
+        glVertex3f(self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glEnd()
+
+        # top face: blue
+        glColor3f(0, 0, 1)
+        glBegin(GL_QUADS)
+        glVertex3f(self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, self.cube_size / 2, self.cube_size / 2)
+        glEnd()
+
+        # bottom face: green
+        glColor3f(0, 1, 0)
+        glBegin(GL_QUADS)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glEnd()
+
+        # left face: white
+        glColor3f(1, 1, 1)
+        glBegin(GL_QUADS)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(-self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glEnd()
+
+        # right face: yellow
+        glColor3f(1, 1, 0)
+        glBegin(GL_QUADS)
+        glVertex3f(self.cube_size / 2, self.cube_size / 2, -self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, self.cube_size / 2)
+        glVertex3f(self.cube_size / 2, -self.cube_size / 2, -self.cube_size / 2)
+        glEnd()
+
+        glPopMatrix()
+
+
+cube = Cube()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        cube.set_cube(1)
+        pygame.display.flip()
+        # pygame.time.wait()
--- display.py
+++ display.py
@@ -7,14 +7,32 @@ from OpenGL.GLU import *
 
 class Display:
     def __init__(self):
-        xrot = yrot = 0
-
+        self.xrot = self.yrot = 0
+        self.flag = False
+        self.k_down = False #TODO maybe remove this one...
 
     def display(self):
+        """
+                This method displays the cube and has to be called in a loop so it is always displaying
+                :param   :
+                :param   :
+                :return: void
+                """
         pass
 
     def update_move(self, move):
+        """
+                This method is called after a move every time so it can update the displaying cube
+                :param move: 90 degree rotation of a specific face
+                of the cube. Brings you into a new display.
+                :return: void
+                """
         pass
 
     def update_hints(self,hints):
+        """
+                This method update the possible hints to a solution
+                :param hints: a list of moves that can be made from a particular state
+                :return: void
+                """
         pass
